b315c117e19c3332f3834391e80d030ddbc27685
Avnish C. Patel <masuroson@gmail.com>
2016-01-08 11:16:03 -0800

    Initial Commit

---
+++ Chip8_Emulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chip8_Emulator
+//
+//  Created by Avnish on 1/8/16.
+//  Copyright © 2016 masuroson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
